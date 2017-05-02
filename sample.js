8017416ef916540e01f4651e46d78d31f03ed6c1
Steven Harradine <stevenharradine@gmail.com>
2017-05-02 00:16:38 -0400

    escape entities

--- sample.js
+++ sample.js
@@ -1,9 +1,9 @@
 module.exports.descriptionlist = [{
 	"term":"Absolute zero",
-	"definition": "The lowest tempature possible where all motion stops, 0K or -273.15°C",
+	"definition": "The lowest tempature possible where all motion stops, 0K or -273.15&deg;C",
 	"category": "Physics"
 },{
 	"term":"Absolute Value",
-	"definition": "Makes negitive values positive and leaves positive numbers unchanged.  Written as |x| and defined as √(x²)",
+	"definition": "Makes negitive values positive and leaves positive numbers unchanged.  Written as |x| and defined as &radic;(x&sup2;)",
 	"category": "Mathematics"
 }]
