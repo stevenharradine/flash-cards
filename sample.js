a7fba748b04aab90e5a8412521c7a0b30f63ef3e
Steven Harradine <stevenharradine@gmail.com>
2017-05-01 23:34:22 -0400

    add sample flashcard file

--- sample.js
+++ sample.js
@@ -1,15 +1,3 @@
-# flash-cards
-Loads a json file with a term and definition and randomly displays a term for you to define.
-
-## usage
-Run the server while passing in your the path of your flashcards file.
-```
-node server sample
-```
-
-## flashcards file format
-Your file should follow the following format
-```
 module.exports.descriptionlist = [{
 	"term":"Absolute zero",
 	"definition": "The lowest tempature possible where all motion stops, 0K or -273.15°C",
@@ -19,4 +7,3 @@ module.exports.descriptionlist = [{
 	"definition": "Makes negitive values positive and leaves positive numbers unchanged.  Written as |x| and defined as √(x²)",
 	"category": "Mathematics"
 }]
-```
